190fff5fbd3e4ebaaa7981e189eb49c97f3cf214
Mary Cotter <marebearc@gmail.com>
2018-07-27 18:56:58 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
-# book.rb
-# Add you Book class here
+class Book
+  
+ attr_reader :title
+ attr_accessor :author, :page_count, :genre
+  def initialize(title)
+  @title = title
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
+booty_bopper = Book.new("Booty Bopper")
+booty_bopper.author = "Yarm Tercot"
+   
